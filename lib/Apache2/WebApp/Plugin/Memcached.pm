2a11d34acf4220a40da49255a6c298e627aaa015
Marc S. Brooks <mbrooks@cpan.org>
2010-01-05 09:38:15 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-Memcached from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-Memcached
    gitpan-cpan-version:      
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-Memcached.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Apache2/WebApp/Plugin/Memcached.pm
+++ lib/Apache2/WebApp/Plugin/Memcached.pm
@@ -20,7 +20,7 @@ use base 'Apache2::WebApp::Plugin';
 use Cache::Memcached;
 use Params::Validate qw( :all );
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
